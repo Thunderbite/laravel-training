e5bbde053450ea964e800252f2dbb415fc54fa1b
Radu Tirnovan <tirnovan.radu@yahoo.com>
2018-01-18 19:19:02 +0200

    @the end@

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -3,17 +3,27 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use App\Models\Text;
+use Illuminate\Database\Query\Builder;
 
 class FrontendController extends Controller
 {
 
+    
     public function index()
     {
+        $fromDate = date('Y-m-d' . ' 00:00:00', time());
+        $toDate = date('Y-m-d' . ' 22:00:40', time());
         $config = [
-            'message' => 'Hello World!'
+            'message' => 'The same thing we do every night, Pinky - try to take over the world!'
         ];
+        $config =[
+             'texts' =>Text::select('keyword', 'text')->whereBetween('created_at', [$fromDate, $toDate] )->get()
+        ]; 
+
 
         // Serve the view
         return view('frontend.index', compact(['config']));
     }
 }
+?>
