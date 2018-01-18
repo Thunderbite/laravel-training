bb89d2bd0742df5894f37ad63a5ccd4e00eb8d74
Radu Tirnovan <tirnovan.radu@yahoo.com>
2018-01-18 19:34:24 +0200

    @the end end@

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -13,12 +13,10 @@ class FrontendController extends Controller
     public function index()
     {
         $fromDate = date('Y-m-d' . ' 00:00:00', time());
-        $toDate = date('Y-m-d' . ' 22:00:40', time());
+        $toDate = date('Y-m-d' . ' 22:00:40', time()); 
         $config = [
-            'message' => 'The same thing we do every night, Pinky - try to take over the world!'
-        ];
-        $config =[
-             'texts' =>Text::select('keyword', 'text')->whereBetween('created_at', [$fromDate, $toDate] )->get()
+            'message' => 'The same thing we do every night, Pinky - try to take over the world!',
+            'texts' =>Text::select('keyword', 'text')->whereBetween('created_at', [$fromDate, $toDate] )->get()
         ]; 
 
 
