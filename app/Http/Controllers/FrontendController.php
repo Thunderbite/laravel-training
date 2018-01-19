b0c96d94d60bbcbd009a6e9cbb7020f40e9ec9d5
Radu Tirnovan <tirnovan.radu@yahoo.com>
2018-01-19 14:14:57 +0200

    Added select dates and validation logic to FrontendController

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -5,21 +5,66 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use App\Models\Text;
 use Illuminate\Database\Query\Builder;
+use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
 
+    protected function getDate( $param )
+    {
+        $request = new Request();
+        return strtotime( $request->query($param, null) );
+    }    
+
+    
+    public function checkDates($dates)
+    {
+        if (strtotime($dates[0])) {
+            $date[]=date('Y-m-d H:i:s', $dates[0]);
+        } else {
+            $date[]=false;
+        }
+        if (strtotime($dates[1])) {
+            $date[]=date('Y-m-d H:i:s', $dates[1]);
+        } else {
+            $date[]=false;
+        }
+        return $date;
+    }    
+    
+    public function getTexts( $from, $to ) 
+    {
+	if ( $from && $to ) {
+		return Text::select('keyword', 'text')
+               ->whereBetween('created_at', [ date('Y-m-d H:i:s', $from), date('Y-m-d H:i:s', $to) ])
+               ->get(); 
+	}
+	if ( $from ) {
+		return Text::select('keyword', 'text')
+               ->where('created_at' > date('Y-m-d H:i:s', $from) )
+               ->get(); 
+	}
+	if ( $to ) {
+		return Text::select('keyword', 'text')
+               ->where('created_at' < date('Y-m-d H:i:s', $to) )
+               ->get(); 
+	}
+	return Text::select('keyword', 'text')
+               ->get(); 
+    }
     
     public function index()
     {
-        $fromDate = date('Y-m-d' . ' 00:00:00', time());
-        $toDate = date('Y-m-d' . ' 22:00:40', time()); 
+
+	$from = $this->getDate( 'fromDate' );
+	$to = $this->getDate( 'toDate' );
+
         $config = [
-            'message' => 'The same thing we do every night, Pinky - try to take over the world!',
-            'texts' =>Text::select('keyword', 'text')->whereBetween('created_at', [$fromDate, $toDate] )->get()
-        ]; 
+            'message' => 'Hello World', 
+	    'texts' => $this->getTexts( $from, $to )
+        ];
 
-
+        
         // Serve the view
         return view('frontend.index', compact(['config']));
     }
